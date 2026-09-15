6a3d636fd9cb0959b18342ae42d7897c7c19a721
Chhotu Pathak <chhotupathak9102@gmail.com>
2026-09-15 21:50:54 +0530

    Linear Seaarch

---
+++ ArrayQuestion/LinearSearch.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int LinearSearch(vector<int> arr, int target){
+
+    for(int i = 0; i < arr.size(); i++){
+            if(arr[i] == target){
+                return i;
+            }
+        }
+
+        return -1;
+}
+
+int main(){
+
+  vector<int> arr = {5,5,7,2,7};
+  int target = 7;
+
+  cout<<"Target found at index "<<LinearSearch(arr,target);
+  return 0;
+}
